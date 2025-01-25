02f849f50e37b7ec2392fc791efc92cef937dd1c
Sumsum Gogoi <sumsumgogoi51@example.com>
2025-01-26 02:05:20 +0530

    loading style

--- client/src/app/loading.tsx
+++ client/src/app/loading.tsx
@@ -1,11 +1,9 @@
 import React from "react";
-import load from "../../public/gif/loading.gif";
-import Image from "next/image";
 import style from "./page.module.scss";
 const Loading = () => {
   return (
     <div className={style.loadingCon}>
-      <Image alt="loading.." src={load} className={style.loadImg} />
+      <div className={style.spinner}></div>
     </div>
   );
 };
